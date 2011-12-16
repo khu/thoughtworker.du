b2886ad0cb359e952ae242d246ea422b1de7bc96
Hu Kai <iamkaihu@gmail.com>
2011-12-16 20:24:18 +0800

    change the css.

--- scripts/douban.books.fav_books.js
+++ scripts/douban.books.fav_books.js
@@ -2,7 +2,7 @@ window["DOUBAN"]["BOOKS"]["FAVBOOKS"] = function() {
 	this.act_with = function(books_for_individual) {
 		var html = "<ul>"
 		for(var idx = 0; idx < books_for_individual.size(); idx++) {
-			html += "<li><a href='" + books_for_individual.get(idx).douban_url + "' target='_blank'>";
+			html += "<li><a href='" + books_for_individual.get(idx).book_url + "' target='_blank'>";
 			html += "<img src='"+ books_for_individual.get(idx).image_url + "'></a></li>";
 		}
 		$("#content").html(html += "</ul>")
