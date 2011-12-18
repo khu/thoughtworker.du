893652d6b998790a5c7a1d0f6f7a32e87fc922ad
Hu Kai <iamkaihu@gmail.com>
2011-12-18 11:01:38 +0800

    added the link to the recent reading.s

--- scripts/douban.books.recent_books.js
+++ scripts/douban.books.recent_books.js
@@ -2,7 +2,7 @@ window["DOUBAN"]["BOOKS"]["RECENTBOOKS"] = function() {
 	this.act_with = function(books_for_individual) {
 		if (books_for_individual.size() > 0) {
 			var douban_id = books_for_individual.get(0).douban_id
-			var html = "<ul class='recent'><li class='people_icon' id=" + douban_id + "></li>"
+			var html = "<ul class='recent'><li class='people_icon'><a  href='http://book.douban.com/people/" + douban_id + "/'  id=" + douban_id + "></a></li>"
 			var recent_read = books_for_individual.size() > 5? 5 : books_for_individual.size()
 
 			for (var i = 0; i < recent_read; i++) {
