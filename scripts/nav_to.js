110b23ceb437d1839c397b2bc198ba7461b34486
FENG Zhichao <flankerfc@gmail.com>
2011-12-20 01:32:44 +1100

    replate tab to white space.

--- scripts/nav_to.js
+++ scripts/nav_to.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-	$("#recent").hide();
-	
-	$("#recent_tab").click(function(){
-		$("#content").toggle();
-		$("#recent").toggle();
-		$("#nav div").attr("class","inactive");
-		$(this).attr("class","active");
-	});
-	
-	$("#read_tab").click(function(){
-		$("#content").toggle();
-		$("#recent").toggle();
-		$("#nav div").attr("class","inactive");
-		$(this).attr("class","active");
-	});
+    $("#recent").hide();
+
+    $("#recent_tab").click(function(){
+        $("#content").toggle();
+        $("#recent").toggle();
+        $("#nav div").attr("class","inactive");
+        $(this).attr("class","active");
+    });
+
+    $("#read_tab").click(function(){
+        $("#content").toggle();
+        $("#recent").toggle();
+        $("#nav div").attr("class","inactive");
+        $(this).attr("class","active");
+    });
 });
