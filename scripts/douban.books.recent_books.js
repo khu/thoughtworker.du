0422a28c392608931e05658b1347d41d91cf4e7a
Hu Kai <iamkaihu@gmail.com>
2011-12-25 15:56:15 +0800

    split the long innocation to several steps.

--- scripts/douban.books.recent_books.js
+++ scripts/douban.books.recent_books.js
@@ -1,10 +1,13 @@
 window["DOUBAN"]["BOOKS"]["RECENTBOOKS"] = function() {
   this.act_with = function(books_for_individual) {
-    if (books_for_individual.size() > 0) {
-      var contact = books_for_individual.get(0).contact;
-      var books = books_for_individual.slice(5);
+    if (books_for_individual.size() == 0) return;
+    
+    var contact = books_for_individual.get(0).contact;
+    var books = books_for_individual.slice(5);
 
-      $("<p></p>").append($("#recent-people-template").tmpl(contact)).append($("<ul class='recent'></ul>").append($("#recent-books-template").tmpl( books ))).appendTo("#recent")
-    }
+    var people_icon_section = $("#recent-people-template").tmpl(contact)
+    var books_section = $("<ul class='recent'></ul>").append($("#recent-books-template").tmpl( books ))
+    
+    $("<p></p>").append(people_icon_section).append(books_section).appendTo("#recent")
   };
 }
