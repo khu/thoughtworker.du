d3bb40f5fda54bec7d7b8315feef2ccc8bae33de
Hu Kai <iamkaihu@gmail.com>
2011-12-25 14:59:30 +0800

    move the template into html.

--- scripts/douban.books.recent_books.js
+++ scripts/douban.books.recent_books.js
@@ -1,28 +1,21 @@
 window["DOUBAN"]["BOOKS"]["RECENTBOOKS"] = function() {
 	this.act_with = function(books_for_individual) {
 		if (books_for_individual.size() > 0) {
-      var twer = books_for_individual.get(0);
-			var douban_id = twer.douban_id
-			var html = "<ul class='recent'>"
-      html += "<li>" + twer.name + "</li>"
-      html += "<li class='people_icon'><a  href='http://book.douban.com/people/" + douban_id + "/'  id=" + douban_id + "></a></li>"
+      		var twer = books_for_individual.get(0);
+			var douban_id = twer.douban_id;
+			
 			var recent_read = books_for_individual.size() > 5? 5 : books_for_individual.size()
-
+			var books = [];
+			
 			for (var i = 0; i < recent_read; i++) {
-				html += "<li><a href='" + books_for_individual.get(i).book_url + "' target='_blank'>";
-				html += "<img src='"+ books_for_individual.get(i).image_url.replace("lpic", "spic") + "'></a></li>";
+					var obj = {
+						book_url: books_for_individual.get(i).book_url,
+						book_cover_image : books_for_individual.get(i).image_url.replace("lpic", "spic")
+					}
+					books.push(obj)
 			}
-			html += "</ul>"
-
-			DOUBAN.getUser({
-				id:douban_id,
-				callback:function(json){
-					var user = DOUBAN.parseUser(json);
-					var icon_url = user.link.icon;
-					$("#" + douban_id).html("<image src=" + icon_url + "/>")
-				} 
-			})
-			$("#recent").append(html)
+			var people = {homepage_url:"http://google.com", people_icon:"http://google.com"}
+			$("<p></p>").append($("#recent-people-template").tmpl(people)).append($("<ul class='recent'></ul>").append($("#recent-books-template").tmpl( books ))).appendTo("#recent")
 		}
 	};	
 }
