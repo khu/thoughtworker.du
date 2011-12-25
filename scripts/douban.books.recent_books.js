c0510231876954dff08db03632f183a8e6c09841
FENG Zhichao <flankerfc@gmail.com>
2011-12-25 18:41:49 +1100

    add book title

--- scripts/douban.books.recent_books.js
+++ scripts/douban.books.recent_books.js
@@ -1,21 +1,10 @@
 window["DOUBAN"]["BOOKS"]["RECENTBOOKS"] = function() {
-	this.act_with = function(books_for_individual) {
-		if (books_for_individual.size() > 0) {
+  this.act_with = function(books_for_individual) {
+    if (books_for_individual.size() > 0) {
       var contact = books_for_individual.get(0).contact;
-			var recent_read = books_for_individual.size() > 5? 5 : books_for_individual.size()
-			var books = [];
-			
-			for (var i = 0; i < recent_read; i++) {
-					var obj = {
-						book_url: books_for_individual.get(i).book_url,
-						book_cover_image : books_for_individual.get(i).image_url.replace("lpic", "spic")
-					}
-					books.push(obj)
-			}
-			var people = {homepage_url: "http://book.douban.com/people/" + contact.id,
-			              people_icon: contact.image_url,
-			              name: contact.name}
-			$("<p></p>").append($("#recent-people-template").tmpl(people)).append($("<ul class='recent'></ul>").append($("#recent-books-template").tmpl( books ))).appendTo("#recent")
-		}
-	};
+      var books = books_for_individual.slice(5);
+
+      $("<p></p>").append($("#recent-people-template").tmpl(contact)).append($("<ul class='recent'></ul>").append($("#recent-books-template").tmpl( books ))).appendTo("#recent")
+    }
+  };
 }
--- scripts/douban.domain.books.js
+++ scripts/douban.domain.books.js
@@ -4,13 +4,15 @@ window["DOUBAN"]["BOOKS"]["DOMAIN"] = {}
 window["DOUBAN"]["BOOKS"]["DOMAIN"]["BOOK"] = function(contact, book) {
   this.image_url = book.link.image.replace("spic", "lpic");
   this.book_url  = book.link.alternate;
+  this.cover_image_url = this.image_url.replace("lpic", "spic");
   this.title = book.title;
 
   this.contact = {};
   this.contact.id = contact.nid;
   this.contact.name = contact.name;
   this.contact.page_url = contact.link.alternate;
-  this.contact.image_url = contact.link.icon;
+  this.contact.book_page_url = "http://book.douban.com/perple/" + contact.nid;
+  this.contact.image_icon = contact.link.icon;
 }
 
 window["DOUBAN"]["BOOKS"]["DOMAIN"]["BOOKS"] = function(books) {
@@ -30,5 +32,8 @@ window["DOUBAN"]["BOOKS"]["DOMAIN"]["BOOKS"] = function(books) {
 
   this.get = function(index) {
     return this.books[index]
+  }
+  this.slice = function(size) {
+    return this.books.slice(0, size);
   }
 }
