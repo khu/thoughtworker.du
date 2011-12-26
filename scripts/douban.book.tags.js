36b8505d62ae240efa8b3ac1f7ae593b4f4620af
Hu Kai <iamkaihu@gmail.com>
2011-12-26 16:25:06 +0800

    be able to render all tags onto the browsers.

---
+++ scripts/douban.book.tags.js
@@ -0,0 +1,16 @@
+var allTags = new DOUBAN.BOOKS.DOMAIN.TAGS()
+
+window["DOUBAN"]["BOOKS"]["TAGS"] = function() {
+    this.act_with = function(books_for_individual) {
+        books_for_individual.foreach(function(book){
+            DOUBAN.getBookTags({
+                id : book.id,
+                callback:function(tagsJson) {
+                    var tagObj = DOUBAN.parseTags(tagsJson);
+                    var tags = new DOUBAN.BOOKS.DOMAIN.TAGS(tagObj)
+                    allTags.merge(tags).renderTo("#tags")
+                }
+            })            
+        })
+    };
+}
--- scripts/douban.domain.books.js
+++ scripts/douban.domain.books.js
@@ -37,4 +37,74 @@ window["DOUBAN"]["BOOKS"]["DOMAIN"]["BOOKS"] = function(books) {
   this.slice = function(size) {
     return this.books.slice(0, size);
   }
+  this.foreach = function(callback) {
+      for(var i = 0; i < this.books.length;i++) {
+          callback(this.books[i])
+      }
+  }
+}
+
+
+window["DOUBAN"]["BOOKS"]["DOMAIN"]["TAG"] = function(link, text, vote) {
+    this.link = link;
+    this.text = text;
+    this.vote = vote;
+    
+    this.increaseVote = function() {
+        this.vote++
+    }
+}
+
+
+window["DOUBAN"]["BOOKS"]["DOMAIN"]["TAGS"] = function(tags) {
+    this.tags = {}
+
+    this.add = function(tag) {
+        if (this.tags[tag.text]) {
+            this.tags[tag.text].increaseVote();
+        } else {
+            this.tags[tag.text] = tag;
+        }
+    }
+    
+    this.is_empty = function() {
+        for(var property in this.tags) {
+            return false;
+        }
+        return true;
+    }
+    
+    if (tags) {
+        for (var i  = 0; i < tags.entries.length;i++) {
+            var entry = tags.entries[i]
+            this.add(new DOUBAN.BOOKS.DOMAIN.TAG(entry.id, entry.title, 1))
+        }
+    };
+    
+    this.foreach = function(callback) {
+        for(var tag in this.tags) {
+            callback(this.tags[tag])
+        }
+    }
+    
+    this.merge = function(tagsObj) {
+        var doubanTag = this;
+        var isEmpty = this.is_empty();
+        if (isEmpty) {
+            this.tags = tagsObj.tags
+            return this;
+        } else {
+            tagsObj.foreach(function(tag){
+                doubanTag.add(tag)
+            })
+            return doubanTag            
+        }
+    }
+    
+    this.renderTo = function(id) {
+        $(id).empty()
+        this.foreach(function(tag){
+            $(id).append($("<li>" + tag.text + "(" + tag.vote + ")</li>"))
+        })
+    }
 }
