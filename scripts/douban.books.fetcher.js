77cda4e9e64b64ea04967c020bb161fa8149ef5f
Jianing Zheng <jnzheng@thoughtworks.com>
2013-03-25 17:50:56 +0800

    display total books quantity on colleagues page for each colleague(douban api v2 supported). jianing

--- scripts/douban.books.fetcher.js
+++ scripts/douban.books.fetcher.js
@@ -1,4 +1,5 @@
 window["DOUBAN"]["BOOKS"]["CONTINUNOUSFETCHER"] = function (fav_books_el, contacts, douban_books_callback) {
+  var MAX_RESULTS = 3;
   this.contacts = contacts;
 
   this.bind_scroll_event = function () {
@@ -33,13 +34,13 @@ window["DOUBAN"]["BOOKS"]["CONTINUNOUSFETCHER"] = function (fav_books_el, contac
       status : 'read',
       callback : function (books) {
         contact.load(books.entry.length);
+        contact.totalBooks = books.total;
         var books_for_individual = new DOUBAN.BOOKS.DOMAIN.BOOKS();
-        var max_results = 3;
-        for (var idx = 0; idx < max_results; idx++) {
+        for (var idx = 0; idx < MAX_RESULTS; idx++) {
           books_for_individual.add(doubanbooks._parse(contact, books.entry[idx]));
         }
         books = books_for_individual.books;
-        for (var i = 0; i < max_results; i++) {
+        for (var i = 0; i < MAX_RESULTS; i++) {
           douban_books_callback[i].act_with(books_for_individual);
         }
       }
@@ -48,6 +49,7 @@ window["DOUBAN"]["BOOKS"]["CONTINUNOUSFETCHER"] = function (fav_books_el, contac
 };
 
 window["DOUBAN"]["BOOKS"]["ONETIMEFETCHER"] = function (fav_books_el, contacts, douban_books_callback) {
+  var MAX_RESULTS = 3;
   this.contacts = contacts;
 
   this._parse = function (contact, book) {
@@ -60,13 +62,13 @@ window["DOUBAN"]["BOOKS"]["ONETIMEFETCHER"] = function (fav_books_el, contacts, 
       uid : contact.id,
       status : 'read',
       callback : function (books) {
+        contact.totalBooks = books.total;
         var books_for_individual = new DOUBAN.BOOKS.DOMAIN.BOOKS();
-        var max_results = 3;
-        for (var idx = 0; idx < max_results; idx++) {
+        for (var idx = 0; idx < MAX_RESULTS; idx++) {
           books_for_individual.add(doubanbooks._parse(contact, books.collections[idx]));
         }
         books = books_for_individual.books;
-        for (var i = 0; i < max_results; i++) {
+        for (var i = 0; i < MAX_RESULTS; i++) {
           douban_books_callback[i].act_with(books_for_individual);
         }
       }
